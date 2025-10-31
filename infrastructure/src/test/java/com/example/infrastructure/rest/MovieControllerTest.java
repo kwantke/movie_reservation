168130d8e283f9690a630b1047fa09502e311b2b
kokk <kokk7310@gmail.com>
2025-10-31 22:32:34 +0900

    feat : 컨트롤러 통합 테스트 작성 - 외부 통합해 주요 흐름 위주로 테스트

---
+++ infrastructure/src/test/java/com/example/infrastructure/rest/MovieControllerTest.java
@@ -0,0 +1,128 @@
+package com.example.infrastructure.rest;
+
+import com.example.application.dto.request.MovieRequestDto;
+import com.example.application.dto.request.ScreeningRequestDto;
+import com.example.domain.model.valueObject.ContentRating;
+import com.example.domain.model.valueObject.Genre;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@DisplayName("[통합 테스트] 조회 컨트롤러")
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest
+class MovieControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @DisplayName("제목으로 영화 검색 시 해당 키워드를 포함한 영화 목록 및 200 응답을 반환한다")
+  @Test
+  void givenTitleKeyword_whenSearchMovie_thenReturn200() throws Exception {
+    mockMvc.perform(get("/api/v1/movies")
+                    .param("title", "10")
+                    .contentType(MediaType.APPLICATION_JSON)
+            ).andExpect(status().isOk())
+            .andExpect(jsonPath("$.length()").isNotEmpty()); //응답 JSON 배열이 비어 있지 않은지 확인
+  }
+
+
+  @DisplayName("장르로 영화 검색시 해당 장르의 영화 목록 및 200 응답을 반환한다.")
+  @Test
+  void givenGenre_whenSearchMovies_thenReturn200WithMovies() throws Exception {
+    mockMvc.perform(get("/api/v1/movie")
+                    .param("genre", "ACTION")
+                    .contentType(MediaType.APPLICATION_JSON)
+            ).andExpect(status().isOk())
+            .andExpect(jsonPath("$.length()").isNotEmpty())
+            .andExpect(jsonPath("$[0].genre").value("ACTION")); // 첫번째 결과의 장르 확인
+  }
+
+  @DisplayName("정상적인 영화 등록 요청 시 201 응답을 반환한다.")
+  @Test
+  void givenValidRequest_whenCreateMovie_thenReturn201() throws Exception {
+    //Given
+    MovieRequestDto validDto = new MovieRequestDto(
+            "New Movie",
+            ContentRating.ADULT,
+            LocalDate.of(2025, 10, 31),
+            "https://example.com/thumbnail.jpg",
+            150,
+            Genre.ACTION
+    );
+
+    // When & Then
+    mockMvc.perform(post("/api/v1/movies")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(validDto)))
+            .andExpect(status().isCreated());
+  }
+
+  @DisplayName("잘못된 영화 등록 요청 시 400 응답을 반환한다.")
+  @Test
+  void givenInvalidRequet_whenCreateMovie_thenReturn400() throws Exception {
+    // Given: 제목이 비어 있음
+    MovieRequestDto invalidDto = new MovieRequestDto(
+            "", // 빈 제목(유효하지 않음)
+            ContentRating.ADULT,
+            LocalDate.of(2025, 10, 31),
+            "https://example.com/thumbnail.jpg",
+            150,
+            Genre.ACTION
+    );
+
+    // When & Then
+    mockMvc.perform(post("/api/v1/movies")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(invalidDto)))
+            .andExpect(status().isBadRequest());
+  }
+
+  @DisplayName("영화 상영 시간 등록 시 201 응답을 반환한다")
+  @Test
+  void givenValidScreeningRequest_whenCreateScreening_thenReturn201() throws Exception {
+    // Given
+    ScreeningRequestDto screeningRequestDto = new ScreeningRequestDto(1L, LocalTime.of(12, 0, 0));
+
+    // When & Then
+    mockMvc.perform(post("/api/v1/movies/1/screeings")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(screeningRequestDto)))
+            .andExpect(status().isCreated());
+  }
+
+  @DisplayName("잘못된 영화 상영 시간 등록 요청 시 400 응답을 반환한다.")
+  @Test
+  void givenInvalidScreeningRequest_whenCreateCscreening_thenReturn400() throws Exception {
+    // Given: 상영관 정보가 유효하지 않음
+    ScreeningRequestDto screeningRequestDto = new ScreeningRequestDto(0L, LocalTime.of(12, 0, 0));
+
+    // When & Then
+    mockMvc.perform(post("/api/v1/movies/1/screening")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(screeningRequestDto)))
+            .andExpect(status().isBadRequest());
+  }
+
+
+}
